c4255bc12a13f14bdab802f39d8e779a6df05c95
Joshua Diaddigo <joshuad95@hotmail.com>
2017-05-30 17:09:25 -0400

    web app improvements

--- internal/dashboard.js
+++ internal/dashboard.js
@@ -1,5 +1,23 @@
 get_prayers();
 
+if (('standalone' in window.navigator) && window.navigator.standalone) {
+    window.addEventListener('load', function() {
+        var links = document.links,
+            link,
+            i;
+
+        for (i = 0; i < links.length; i++) {
+            // Don't do this for javascript: links
+            if (~(link = links[i]).href.toLowerCase().indexOf('javascript')) {
+                link.addEventListener('click', function(event) {
+                    top.location.href = this.href;
+                    event.returnValue = false;
+                }, false);
+            }
+        }
+    }, false);
+}
+
 function get_prayers() {
     new jsh.Request({
         url: "db/get_prayer.php",
