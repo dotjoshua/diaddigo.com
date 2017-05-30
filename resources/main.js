50586d6fab15e3b26d96496e121ef2a26d38ae27
Joshua Diaddigo <joshuad95@hotmail.com>
2017-05-30 16:51:05 -0400

    modernizing page open

--- resources/main.js
+++ resources/main.js
@@ -10,7 +10,7 @@ var alert = function(a, b, c) {
 window.onload = function() {
     jsh.cm();
 
-    open_page("page_login");
+    jsh.pages["login"].open();
 
     jsh.get("#login_password").addEventListener("keypress", login_keypress_handler);
     jsh.get("#login_username").addEventListener("keypress", login_keypress_handler);
@@ -80,51 +80,11 @@ function load_dashboard(token, silent) {
                 css.innerHTML = response["css"];
                 document.head.appendChild(css);
 
-                open_page("page_dashboard");
+                jsh.pages["dashboard"].open();
                 new jsh.Alert().close();
             }
         }
     }).post();
-}
-
-function open_page(page_div_id) {
-    if (jsh.get("#" + page_div_id) == undefined) {
-        alert("Page does not exist.", "Oops!");
-        return;
-    }
-
-    if (current_page == page_div_id) {
-        return;
-    }
-
-    current_page = page_div_id;
-
-    var pages = jsh.get(".page");
-    for (var i in pages) {
-        if (!pages.hasOwnProperty(i)) continue;
-        pages[i].classList.add("transparent");
-        pages[i].classList.remove(pages[i].id + "_loading");
-    }
-
-    setTimeout(function() {
-        var pages = jsh.get(".page");
-        for (var i in pages) {
-            if (!pages.hasOwnProperty(i)) continue;
-            pages[i].classList.add("display_none");
-        }
-
-        jsh.get("#" + page_div_id).classList.remove("display_none");
-        jsh.get("#" + page_div_id).classList.add(page_div_id + "_loading");
-        setTimeout(function() {
-            jsh.get("#" + page_div_id).classList.remove("transparent");
-        }, 10);
-
-        setTimeout(function() {
-            window.dispatchEvent(new Event('page_opened'));
-        }, 500);
-    }, 500);
-
-    window.location.hash = page_div_id.substring(5);
 }
 
 if (('standalone' in window.navigator) && window.navigator.standalone) {
@@ -137,7 +97,7 @@ if (('standalone' in window.navigator) && window.navigator.standalone) {
             // Don't do this for javascript: links
             if (~(link = links[i]).href.toLowerCase().indexOf('javascript')) {
                 link.addEventListener('click', function(event) {
-                    window.location.href = this.href;
+                    top.location.href = this.href;
                     event.returnValue = false;
                 }, false);
             }
